8177aff3c6301ab1807fcfeba5f4ec5a38d84b97
Thomas Czachorowski <Thomas.Czachorowski@cdpipelinedevshop.com>
2022-08-30 07:59:54 -0400

    Commit 2

--- src/main/java/CheckingAccount.java
+++ src/main/java/CheckingAccount.java
@@ -2,7 +2,22 @@ public class CheckingAccount {
 
     public double balance;
     public double interestRate = 0.01;
+    public double withdrawal;
     public String name;
+
+    public void with() {
+        if (withdrawal>1 && withdrawal<balance) {
+            System.out.println("Withdrawal Successful");
+            balance-=withdrawal;
+        }
+        else if (withdrawal < 0) {
+            System.out.println("Invalid Number");
+        }
+        else{
+            System.out.println("Not enough Money in account");
+
+        }
+    }
 
     public void displayCustomer(){
         System.out.println("Customer: "+name);
--- src/test/java/AccountTest.java
+++ src/test/java/AccountTest.java
@@ -2,13 +2,18 @@ public class AccountTest {
 
     public static void main(String[] args) {
         CheckingAccount sa0001 = new CheckingAccount();
-        sa0001.balance = 1000;
+        sa0001.balance = 1200;
         sa0001.name = "Damien";
         sa0001.interestRate = 0.02;
+        sa0001.withdrawal = -1;
+
+        sa0001.with();
 
         CheckingAccount sa0002 = new CheckingAccount();
         sa0002.balance = 2000;
         sa0002.name = "Bill";
-        
+
+        sa0001.displayCustomer();
+        sa0002.displayCustomer();
     }
 }
