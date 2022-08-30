195f3adfeea596b08c6ece875618139844fce23b
Thomas Czachorowski <Thomas.Czachorowski@cdpipelinedevshop.com>
2022-08-30 07:43:14 -0400

    Initial commit

---
+++ src/main/java/CheckingAccount.java
@@ -0,0 +1,10 @@
+public class CheckingAccount {
+
+    public double balance;
+    public double interestRate = 0.01;
+    public String name;
+
+    public void displayCustomer(){
+        System.out.println("Customer: "+name);
+    }
+}
---
+++ src/test/java/AccountTest.java
@@ -0,0 +1,14 @@
+public class AccountTest {
+
+    public static void main(String[] args) {
+        CheckingAccount sa0001 = new CheckingAccount();
+        sa0001.balance = 1000;
+        sa0001.name = "Damien";
+        sa0001.interestRate = 0.02;
+
+        CheckingAccount sa0002 = new CheckingAccount();
+        sa0002.balance = 2000;
+        sa0002.name = "Bill";
+        
+    }
+}
